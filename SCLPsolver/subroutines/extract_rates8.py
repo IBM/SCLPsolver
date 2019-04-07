b269cd96daf23e2b8c27635a2573e128707acf28
ev_shindin <evgensh@il.ibm.com>
2019-04-07 18:16:21 +0300

    data generators

--- SCLPsolver/subroutines/extract_rates8.py
+++ SCLPsolver/subroutines/extract_rates8.py
@@ -1,6 +1,6 @@
 import numpy as np
 from .matlab_utils import find
-from .sparse_matrix_constructor import sparse_matrix_constructor
+from .matrix_constructor8 import matrix_constructor
 from .pivot import base_pivot
 
 
@@ -13,21 +13,19 @@ def extract_rates_from_basis(prim_name, dual_name, dct, problem_dims):
     if problem_dims.KK < problem_dims.totalK:
         kn2 =  dual_name[dual_name > 0]
         kord = np.argsort(np.argsort(np.hstack((kn1, kn2))))[:len(kn1)]
-        dx = sparse_matrix_constructor(dct[klist1+1,0], kord, problem_dims.KK)
+        dx = (dct[klist1+1,0], kord)
     else:
-        dx = sparse_matrix_constructor(dct[klist1+1,0], kn1-1, problem_dims.KK)
+        dx = (dct[klist1+1,0], kn1-1)
     if problem_dims.JJ < problem_dims.totalJ:
         jn1 = -prim_name[prim_name < 0]
         jord = np.argsort(np.argsort(np.hstack((jn1, jn2))))[len(jn1):]
-        dq = sparse_matrix_constructor(dct[0,jlist2+1], jord, problem_dims.JJ)
+        dq = (dct[0,jlist2+1], jord)
     else:
-        dq = sparse_matrix_constructor(dct[0,jlist2+1], jn2-1, problem_dims.JJ)
+        dq = (dct[0,jlist2+1], jn2-1)
     return dx, dq
 
 def extract_rates_from_subproblem(pivots, AAN1, AAN2, problem_dims):
     # Warning this based on assumption that first basis in new_base_sequence is equal to the AAN1 and/or last basis is equal to the AAN2
-    dx = sparse_matrix_constructor(None, None, problem_dims.KK)
-    dq = sparse_matrix_constructor(None, None, problem_dims.JJ)
     if len(pivots) > 0:
         if AAN1 is not None:
             pm1 = AAN1['prim_name'].copy()
@@ -35,14 +33,20 @@ def extract_rates_from_subproblem(pivots, AAN1, AAN2, problem_dims):
             DD1 = AAN1['A'].copy()
             if AAN2 is not None:
                 ran = enumerate(pivots[:-1])
+                dx = matrix_constructor(None, None, problem_dims.KK, -1, len(pivots))
+                dq = matrix_constructor(None, None, problem_dims.JJ, -1, len(pivots))
             else:
                 ran = enumerate(pivots)
+                dx = matrix_constructor(None, None, problem_dims.KK, -1, len(pivots)+1)
+                dq = matrix_constructor(None, None, problem_dims.JJ, -1, len(pivots)+1)
             for i, piv1 in ran:
                 DD1, pm1, dm1 = base_pivot(DD1, find(pm1 == piv1[0])[0], find(dm1 == piv1[1])[0], pm1, dm1)
                 ndx, ndq = extract_rates_from_basis(pm1, dm1, DD1, problem_dims)
                 dx.append(ndx)
                 dq.append(ndq)
         else:
+            dx = matrix_constructor(None, None, problem_dims.KK, 1, len(pivots) + 1)
+            dq = matrix_constructor(None, None, problem_dims.JJ, 1, len(pivots) + 1)
             pm1 = AAN2['prim_name'].copy()
             dm1 = AAN2['dual_name'].copy()
             DD1 = AAN2['A'].copy()
@@ -51,4 +55,7 @@ def extract_rates_from_subproblem(pivots, AAN1, AAN2, problem_dims):
                 ndx, ndq = extract_rates_from_basis(pm1, dm1, DD1, problem_dims)
                 dx.prepend(ndx)
                 dq.prepend(ndq)
-    return dx, dq
+    else:
+        dx = matrix_constructor(None, None, problem_dims.KK, -1, 1)
+        dq = matrix_constructor(None, None, problem_dims.JJ, -1, 1)
+    return dx.get_matrix(), dq.get_matrix()
