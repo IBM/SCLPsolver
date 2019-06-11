0a8472f819a1142107f81dee3a85beefc77d5fca
Evgeny Shindin <>
2019-06-11 13:02:51 +0300

    starting equation updater

---
+++ SCLPsolver/subroutines/equation_updater.py
@@ -0,0 +1,22 @@
+
+
+class equation_updater():
+
+    def __init__(self, param_line):
+        self._indexes = [0]
+        self._matrix = [1]
+        self.rhs = []
+        self.drhs = []
+        self.tau = []
+        self.dtau = []
+
+    def update_solution(self, param_line):
+        pass
+
+    def _add_column(self, n, col):
+        pass
+
+    def _add_row(self, row):
+        pass
+
+
