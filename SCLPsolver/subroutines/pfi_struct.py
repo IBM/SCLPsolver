c856d0cb71b31151cc174a95b4285f619768a9a5
Evgeny Shindin <>
2019-07-31 17:26:22 +0300

    more comments on pfi_struct

--- SCLPsolver/subroutines/pfi_struct.py
+++ SCLPsolver/subroutines/pfi_struct.py
@@ -7,26 +7,26 @@ class pfi_struct():
     # prim_names - vector
     # dual_names - vector
     # place - scalar - place of the matrix in the base sequence
-    # pivots - object of the pivot_storage class (store nnames of entering and leaving variables)
+    # pivots - object of the pivot_storage class (store names of entering and leaving variables)
     # eta_rows - eta vectors to perform pivoting on dual variables and matrix rows
     # eta_cols - eta vectors to perform pivoting on primal variables and matrix columns
     __slots__ = ['_simplex_dict','_prim_names', '_dual_names', '_place', '_pivots', '_eta_rows', '_eta_cols']
 
     # should return row of the simplex dictionary performing sequence of ftran operations
     # n-place number of operations to perform
-    # var_name  of primal variable which index+1 indicates row of the matrix
+    # var_name is an element of primal variable we should take index+1 that indicates row of the matrix
     def get_dict_row_at(self, n, var_name):
         pass
 
-    # should return column of the simplex dictionary performing sequence of ftran operations
+    # same for columns
     def get_dict_col_at(self, n, var_name):
         pass
 
-    # should return primal variables performing sequence of ftran operations
+    # same for first column of matrix
     def get_prim_vars_at(self, n):
         pass
 
-    # should return dual variables performing sequence of ftran operations
+    # same for first row of matrix
     def get_dual_vars_at(self, n):
         pass
 
