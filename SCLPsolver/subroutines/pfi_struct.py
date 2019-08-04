e85ca0099ae91656e6955a969de5703fe2974283
Ari Volcoff <ariv@il.ibm.com>
2019-08-04 17:03:41 +0300

    added implementation for get_dict_row_at

--- SCLPsolver/subroutines/pfi_struct.py
+++ SCLPsolver/subroutines/pfi_struct.py
@@ -1,8 +1,7 @@
+import numpy as np
+import timeit
 
-
-
-class pfi_struct():
-
+class pfi_struct:
     # simplex_dict - matrix
     # prim_names - vector
     # dual_names - vector
@@ -10,24 +9,61 @@ class pfi_struct():
     # pivots - object of the pivot_storage class (store names of entering and leaving variables)
     # eta_rows - eta vectors to perform pivoting on dual variables and matrix rows
     # eta_cols - eta vectors to perform pivoting on primal variables and matrix columns
-    __slots__ = ['_simplex_dict','_prim_names', '_dual_names', '_place', '_pivots', '_eta_rows', '_eta_cols']
+
+    # __slots__ = ['simplex_dict', 'prim_names', 'dual_names', 'place', 'pivots', 'eta_rows', 'eta_cols']
+
+    def __init__(self,simple_dict, prim_names, dual_names, place, eta_rows, eta_cols):
+        self.simple_dict = simple_dict
+        self.prim_names = prim_names
+        self.dual_names = dual_names
+        self.place = place
+        self.eta_rows = eta_rows
+        self.eta_cols = eta_cols
 
     # should return row of the simplex dictionary performing sequence of ftran operations
     # n-place number of operations to perform
-    # var_name is an element of primal variable we should take index+1 that indicates row of the matrix
+    # varname is an element of primal variable we should take index+1 that indicates row of the matrix
     def get_dict_row_at(self, n, var_name):
-        pass
+        # 1 choose from  a row from the matrix (taking the values vector) based on the index of the prim_names that
+        # correspond to the var_name
+
+        # 2 take the corresponding eta row based on the order in the vector
+        result = None
+        index = 0
+        for eta_row in self.eta_rows:
+            # 3 push these inputs into the ftran() function
+
+            index+=index+1
+
+            if result == None:
+                index_of_row_to_pivot = np.where(self.prim_names == var_name)[0][0]
+                values_vector = self.simple_dict[index_of_row_to_pivot]
+                result = ftran(self.prim_names, values_vector, eta_row, index_of_row_to_pivot, var_name)
+            else:
+                primal_names_vector = result[0]
+                primal_values_vector = result[1]
+                index_of_row_to_pivot = np.where(primal_names_vector == var_name)[0][0]
+                result = ftran(primal_names_vector, primal_values_vector, eta_row, index_of_row_to_pivot, var_name)
+
+            # 4 output of the ftran should be fed again to the ftran as input until iteration == n-place
+            if (index <= n - self.place):
+                break
+
+        return result
 
     # same for columns
     def get_dict_col_at(self, n, var_name):
+        # same as above but using the eta_cols and the column of the matrix, and using dual_names instead of prim_names
         pass
 
     # same for first column of matrix
     def get_prim_vars_at(self, n):
+        # similar to 1st method but using the 1st column of the matrix
         pass
 
     # same for first row of matrix
     def get_dual_vars_at(self, n):
+        # similar to 1st method but using the 1st row of the matrix
         pass
 
     # should remove data between n1 and n2 positions
@@ -41,3 +77,39 @@ class pfi_struct():
     # should replace data between n1 and n2 positions many
     def replace_to_many(self, n1, n2, struct):
         pass
+
+
+def ftran(names_vector, values_vector, eta_vector, pivot_index, entering_var_name):
+    t = timeit.Timer('char in text', setup='text = "sample string"; char = "g"')
+
+    # handle primal names vector
+    index_of_location_to_insert_entering_variable = np.searchsorted(names_vector, entering_var_name)
+    pivot_element_value = values_vector[pivot_index] * eta_vector[pivot_index]
+    values_vector += values_vector[pivot_index] * eta_vector
+
+    if index_of_location_to_insert_entering_variable > pivot_index:
+        # handle names vector
+        names_vector[pivot_index: index_of_location_to_insert_entering_variable - 1] = names_vector[pivot_index + 1: index_of_location_to_insert_entering_variable]
+        names_vector[index_of_location_to_insert_entering_variable - 1] = entering_var_name
+
+        # handle values vector
+        values_vector[pivot_index: index_of_location_to_insert_entering_variable - 1] = values_vector[pivot_index + 1: index_of_location_to_insert_entering_variable]
+        values_vector[index_of_location_to_insert_entering_variable - 1] = pivot_element_value
+    else:
+        # handle names vector
+        names_vector[index_of_location_to_insert_entering_variable] = entering_var_name
+        names_vector[index_of_location_to_insert_entering_variable + 1: pivot_index] = names_vector[index_of_location_to_insert_entering_variable: pivot_index]
+
+        # handle values vector
+        values_vector[index_of_location_to_insert_entering_variable] = pivot_element_value
+        values_vector[index_of_location_to_insert_entering_variable + 1: pivot_index] = values_vector[index_of_location_to_insert_entering_variable: pivot_index]
+
+    print('time taken in milliseconds =', t.timeit()/1000)
+
+    print('new_primal_names_vector =', names_vector)
+    print('new_primal_values_vector =', values_vector)
+
+    return [names_vector, values_vector]
+
+pfi_instance = pfi_struct(np.asarray([[1, 2],[ 3, 4]]),np.asarray([1, 2]), np.asarray([3, 4]), 2, np.asarray([[1, 2],[3, 4]]), np.asarray([[1, 2],[3, 4]]))
+print(pfi_instance.get_dict_row_at(1, 2))
