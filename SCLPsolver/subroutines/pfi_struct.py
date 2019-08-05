9be3cfb7401427c955cdca9a2c42cb9d1cbc2093
Ari Volcoff <ariv@il.ibm.com>
2019-08-05 15:00:55 +0300

    fixed initialisation matrix

--- SCLPsolver/subroutines/pfi_struct.py
+++ SCLPsolver/subroutines/pfi_struct.py
@@ -92,7 +92,6 @@ class pfi_struct:
 
 
 def ftran(names_vector, values_vector, eta_vector, pivot_index, entering_var_name):
-    t = timeit.Timer('char in text', setup='text = "sample string"; char = "g"')
 
     # handle primal names vector
     index_of_location_to_insert_entering_variable = np.searchsorted(names_vector, entering_var_name)
@@ -117,15 +116,15 @@ def ftran(names_vector, values_vector, eta_vector, pivot_index, entering_var_nam
         values_vector[index_of_location_to_insert_entering_variable] = pivot_element_value
 
 
-    print('time taken in milliseconds =', t.timeit()/1000)
 
-    print('new_primal_names_vector =', names_vector)
-    print('new_primal_values_vector =', values_vector)
 
     return [names_vector, values_vector]
 
+t = timeit.Timer('char in text', setup='text = "sample string"; char = "g"')
 prim_names = np.asarray([1, 2, 3, 4])
 exiting_names = np.asarray([-1, -2,-3,-4])
 pivots = pivot_storage(prim_names,exiting_names)
-pfi_instance = pfi_struct(np.asarray([[1, 2, 3, 4], [5, 6, 7, 8]]), prim_names, np.asarray([5,6,7,8]), 2, np.asarray([[1, 2, 3, 4],[5, 6, 7, 8]]), np.asarray([[1, 2, 3, 4],[5, 6, 7, 8]]),pivots)
+pfi_instance = pfi_struct(np.asarray([[1, 2, 3, 4], [5, 6, 7, 8], [1, 2, 3, 4], [1, 2, 3, 4]]), prim_names, np.asarray([5,6,7,8]), 2, np.asarray([[1, 2, 3, 4],[5, 6, 7, 8]]), np.asarray([[1, 2, 3, 4],[5, 6, 7, 8]]),pivots)
 print(pfi_instance.get_dict_row_at(1, 2))
+print(pfi_instance.get_dict_col_at(1, 6))
+print('time taken in milliseconds =', t.timeit()/1000)
