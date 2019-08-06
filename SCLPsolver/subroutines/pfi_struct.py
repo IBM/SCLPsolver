96785aa23e4ba510b3e580d7804337d9b0d90674
Ari Volcoff <ariv@il.ibm.com>
2019-08-06 14:58:23 +0300

    moved the tests out of the code itself

--- SCLPsolver/subroutines/pfi_struct.py
+++ SCLPsolver/subroutines/pfi_struct.py
@@ -1,6 +1,4 @@
 import numpy as np
-import timeit
-from pivot_storage import pivot_storage
 
 class pfi_struct:
     # simplex_dict - matrix
@@ -99,7 +97,6 @@ class pfi_struct:
     def replace_to_many(self, n1, n2, struct):
         pass
 
-
 def ftran(names_vector, values_vector, eta_vector, pivot_index, entering_var_name):
 
     # handle primal names vector
@@ -125,18 +122,3 @@ def ftran(names_vector, values_vector, eta_vector, pivot_index, entering_var_nam
         values_vector[index_of_location_to_insert_entering_variable] = pivot_element_value
 
     return [names_vector, values_vector]
-
-
-
-t = timeit.Timer('char in text', setup='text = "sample string"; char = "g"')
-
-prim_names = np.asarray([1, 2, 3, 4])
-exiting_names = np.asarray([-1, -2,-3,-4])
-pivots = pivot_storage(prim_names,exiting_names)
-pfi_instance = pfi_struct(np.asarray([[1, 2, 3, 4], [5, 6, 7, 8], [1, 2, 3, 4], [1, 2, 3, 4]]), prim_names, np.asarray([5,6,7,8]), 2, np.asarray([[1, 2, 3, 4],[5, 6, 7, 8]]), np.asarray([[1, 2, 3, 4],[5, 6, 7, 8]]),pivots)
-print(pfi_instance.get_dict_row_at(1, 2))
-print(pfi_instance.get_dict_col_at(1, 6))
-print(pfi_instance.get_prim_vars_at(1))
-print(pfi_instance.get_dual_vars_at(1))
-
-print('time taken in milliseconds =', t.timeit()/1000)
---
+++ SCLPsolver/tests/pfi_struct_test.py
@@ -0,0 +1,16 @@
+import timeit
+from subroutines.pivot_storage import *
+from subroutines.pfi_struct import *
+
+t = timeit.Timer('char in text', setup='text = "sample string"; char = "g"')
+
+prim_names = np.asarray([1, 2, 3, 4])
+exiting_names = np.asarray([-1, -2,-3,-4])
+pivots = pivot_storage(prim_names,exiting_names)
+pfi_instance = pfi_struct(np.asarray([[1, 2, 3, 4], [5, 6, 7, 8], [1, 2, 3, 4], [1, 2, 3, 4]]), prim_names, np.asarray([5,6,7,8]), 2, np.asarray([[1, 2, 3, 4],[5, 6, 7, 8]]), np.asarray([[1, 2, 3, 4],[5, 6, 7, 8]]),pivots)
+print(pfi_instance.get_dict_row_at(1, 2))
+print(pfi_instance.get_dict_col_at(1, 6))
+print(pfi_instance.get_prim_vars_at(1))
+print(pfi_instance.get_dual_vars_at(1))
+
+print('time taken in milliseconds =', t.timeit()/1000)
