3acabe9cdfc7a8fe7a5bc85542a5bc1f4a92e367
Ari Volcoff <ariv@il.ibm.com>
2019-08-12 10:44:52 +0300

    added ability to time the algorithm over many iterations to get a better sense of which algorithm is faster

--- SCLPsolver/tests/matrix_test.py
+++ SCLPsolver/tests/matrix_test.py
@@ -1,6 +1,6 @@
 from subroutines.matrix import matrix
 import numpy as np
-import time
+import timeit
 
 a = 8
 b = 6
@@ -37,6 +37,7 @@ print(test_matrix.get_matrix())
 
 
 matrix_a_size = 200
+times_to_run = 100
 # comparing numpy inverse speed to new algorithm speed
 # 1st testing the numpy implementation
 matrix_a = np.random.randint(10, size=(matrix_a_size, matrix_a_size))
@@ -50,22 +51,21 @@ matrix_to_inverse[0:matrix_a_size, matrix_a_size] = vector_b
 matrix_to_inverse[matrix_a_size, 0:matrix_a_size] = vector_c
 matrix_to_inverse[matrix_a_size, matrix_a_size] = scalar_d
 
-start_time = time.time()
 numpy_inverse_update_matrix = np.linalg.inv(matrix_to_inverse)
-numpy_inverse_time = time.time() - start_time
-print("numpy took ",numpy_inverse_time," seconds")
 
+numpy_inverse_time=timeit.timeit(lambda: 'np.linalg.inv(matrix_a)', number=times_to_run)
+print("numpy took ",numpy_inverse_time," seconds")
 inversed_matrix_a = np.linalg.inv(matrix_a)
 
 c = matrix(None, len(inversed_matrix_a)*2)
 
-start_time = time.time()
+new_algorithm_time = timeit.timeit(lambda: 'c.inverseUpdate2(inversed_matrix_a, vector_b, vector_c, scalar_d)', number=times_to_run)
 improved_algorithm_inverse_update_matrix = c.inverseUpdate2(inversed_matrix_a, vector_b, vector_c, scalar_d)
-new_algorithm_time = time.time() - start_time
-print("new algorithm took ",time.time() - start_time," seconds")
+
+print("new algorithm took ",new_algorithm_time," seconds")
 print("new algorithm is ",numpy_inverse_time-new_algorithm_time," faster")
-print("Numpy result:\n",numpy_inverse_update_matrix)
-print("New Algorithm result:\n",improved_algorithm_inverse_update_matrix)
+#print("Numpy result:\n",numpy_inverse_update_matrix)
+#print("New Algorithm result:\n",improved_algorithm_inverse_update_matrix)
 print(np.array_equal(numpy_inverse_update_matrix,improved_algorithm_inverse_update_matrix))
 
 
