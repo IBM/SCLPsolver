8388c6725f7ef7f6f83c3a74baf8b36540232134
Ari Volcoff <ariv@il.ibm.com>
2019-08-15 13:16:14 +0300

    iterating on the same matrix multiple times

--- SCLPsolver/tests/equation_solver_test.py
+++ SCLPsolver/tests/equation_solver_test.py
@@ -5,7 +5,7 @@ import random
 import time
 
 matrix_size = 300
-times_to_run = 100
+times_to_run = 5
 # comparing numpy inverse speed to new algorithm speed
 # 1st testing the numpy implementation
 # matrix_1 = 10*np.random.rand(matrix_size, matrix_size)
@@ -25,10 +25,18 @@ numpy_algorithm_time = 0
 new_algorithm_time = 0
 
 for i in range(times_to_run):
-    matrix_1 = 10 * np.random.rand(matrix_size, matrix_size)
+
     vector_2 = 10 * np.random.normal(size=matrix_size)
-    matrix_3 = np.linalg.inv(matrix_1)
-    result_4 = np.dot(matrix_3, vector_2)
+
+    if i == 0:
+        matrix_1 = 10 * np.random.rand(matrix_size, matrix_size)
+
+        matrix_3 = np.linalg.inv(matrix_1)
+        result_4 = np.dot(matrix_3, vector_2)
+
+        # step 9
+        equation_solver_9 = equation_solver(matrix_size)
+        equation_solver_9.set_inverse_matrix(matrix_3)
 
     index_to_replace = random.randint(0, matrix_size - 1)
 
@@ -36,18 +44,14 @@ for i in range(times_to_run):
     random_column_vector_5_2 = 10 * np.random.normal(size=matrix_size)
     # step 6
     matrix_1[:, index_to_replace] = random_column_vector_5_2
-    matrix_1[index_to_replace,:] = random_row_vector_5_1
+    matrix_1[index_to_replace, :] = random_row_vector_5_1
 
     # step 7
     start_time = time.time()
     matrix_7 = np.linalg.inv(matrix_1)
     # step 8
-    result_8 = np.dot(matrix_7,vector_2)
+    result_8 = np.dot(matrix_7, vector_2)
     numpy_algorithm_time += time.time() - start_time
-
-    # step 9
-    equation_solver_9 = equation_solver(matrix_size)
-    equation_solver_9.set_inverse_matrix(matrix_3)
 
     start_time = time.time()
     #step 10
@@ -56,6 +60,8 @@ for i in range(times_to_run):
     #print(np.allclose(result10, matrix_7))
     result_11 = equation_solver_9.resolve(vector_2)
     new_algorithm_time += time.time() - start_time
+
+    print("Are Numpy and new Algorithm results the same? :", np.allclose(result_8, result_11))
 
 
 print("Numpy algorithm took ", numpy_algorithm_time, " seconds")
@@ -66,7 +72,6 @@ print("** New algorithm is ",abs(round((size_comparison-1)*100,1)), "%" ,'faster
 #print(result_8)
 #print(result_11)
 
-print("Are Numpy and new Algorithm results the same? :",  np.allclose(result_8,result_11))
 
 
 
