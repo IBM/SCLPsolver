0c700d768592f6bf41f8df2cf8cf5ca9bc414df6
Ari Volcoff <ariv@il.ibm.com>
2019-08-19 10:41:54 +0300

    temp check-in while trying to figure out why both algorithms do not reach the same result

--- SCLPsolver/tests/equation_solver_test.py
+++ SCLPsolver/tests/equation_solver_test.py
@@ -92,11 +92,12 @@ result_8 = np.dot(matrix_7, vector_2)
 numpy_algorithm_time += time.time() - start_time
 
 
+# step 9
 equation_solver_9 = equation_solver(matrix_size)
 equation_solver_9.set_inverse_matrix(matrix_3)
 
 start_time = time.time()
-# step 9
+
 equation_solver_9.add_equation(index_to_replace,index_to_replace,random_row_vector_5_1,random_column_vector_5_2)
 
 #step 10
@@ -112,6 +113,9 @@ print("Numpy algorithm took ", numpy_algorithm_time, " seconds")
 print("New algorithm took ", new_algorithm_time, " seconds")
 size_comparison = numpy_algorithm_time/new_algorithm_time
 print("** New algorithm is ",abs(round((size_comparison-1)*100,1)), "%" ,'faster' if (size_comparison >= 1) else 'slower')
+
+print("results using numpy:\n",result_8)
+print("results using add equation:\n",result_11)
 
 # test remove equation
 
