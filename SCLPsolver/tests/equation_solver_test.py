83debdd83824c7f14b75132bfb0ab66e9d379304
Ari Volcoff <ariv@il.ibm.com>
2019-08-20 11:13:25 +0300

    removed code that is not being used

--- SCLPsolver/tests/equation_solver_test.py
+++ SCLPsolver/tests/equation_solver_test.py
@@ -4,7 +4,7 @@ import numpy as np
 import random
 import time
 
-matrix_size = 3
+matrix_size = 300
 times_to_run = 5
 print('\n test replace_equation')
 
@@ -88,9 +88,11 @@ matrix_2[matrix_size,:] = random_row_vector_5_1
 # matrix_2[index_to_replace+1:, :index_to_replace] = matrix_1[index_to_replace:,:index_to_replace]
 # matrix_2[index_to_replace+1:,index_to_replace+1:] = matrix_1[index_to_replace:,index_to_replace:]
 #
+#
 # matrix_2[index_to_replace,:] = random_row_vector_5_1
 # matrix_2[:,index_to_replace] = random_column_vector_5_2
-
+#
+# res22 = np.linalg.inv(matrix_2)
 
 # step 7
 start_time = time.time()
@@ -103,16 +105,10 @@ numpy_algorithm_time += time.time() - start_time
 # step 9
 equation_solver_9 = equation_solver(matrix_size*2)
 equation_solver_9.set_inverse_matrix(matrix_3)
-equation_solver_10 = equation_solver(matrix_size*2)
-equation_solver_10.set_inverse_matrix(res22)
-equation_solver_10._replace_equation(3, 3, random_row_vector_5_1.copy(), random_column_vector_5_2.copy())
-result_12 = equation_solver_10.resolve(vector_2.copy())
 
 start_time = time.time()
 
-#equation_solver_9.add_equation(index_to_replace,index_to_replace,random_row_vector_5_1,random_column_vector_5_2)
-equation_solver_9.add_equation(matrix_size,matrix_size,random_row_vector_5_1.copy(),random_column_vector_5_2.copy())
-
+equation_solver_9.add_equation(matrix_size,matrix_size,random_row_vector_5_1,random_column_vector_5_2)
 result_11 = equation_solver_9.resolve(vector_2.copy())
 
 new_algorithm_time += time.time() - start_time
@@ -124,11 +120,12 @@ print("Numpy algorithm took ", numpy_algorithm_time, " seconds")
 print("New algorithm took ", new_algorithm_time, " seconds")
 size_comparison = numpy_algorithm_time/new_algorithm_time
 print("** New algorithm is ",abs(round((size_comparison-1)*100,1)), "%" ,'faster' if (size_comparison >= 1) else 'slower')
-
-print("results using numpy:\n",result_8)
-print("results using add equation:\n",result_11)
+#
+# print("results using numpy:\n",result_8)
+# print("results using add equation:\n",result_11)
 
 # test remove equation
+
 
 # test that do replace, add and remove several times
 
