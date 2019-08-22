748dd373bac98375de126fa749007c7ac0f2090b
Ari Volcoff <ariv@il.ibm.com>
2019-08-22 13:00:41 +0300

    changed test based on new requirements

--- SCLPsolver/tests/equation_solver_test.py
+++ SCLPsolver/tests/equation_solver_test.py
@@ -5,7 +5,7 @@ import random
 import time
 
 
-matrix_size = 400
+matrix_size = 4
 times_to_run = 10
 
 print('\n test reverse_vector_order')
@@ -19,72 +19,52 @@ print('result : ', equation_solver_test.reverse_vector_order(vector, order))
 
 # # test that do replace, add and remove several times
 
-def test_replace(inputMatrix):
+def test_replace(input_matrix, equation_solver_parameter):
     print(' testing replace equation')
-    inputMatrixSize = len(inputMatrix)
-    numpy_algorithm_time = 0
-    new_algorithm_time = 0
-    vector_2 = 10 * np.random.normal(size=inputMatrixSize)
+    input_matrix_size = len(input_matrix)
+    vector_2 = 10 * np.random.normal(size=input_matrix_size)
 
-    matrix_3 = np.linalg.inv(inputMatrix)
+    index_to_replace = random.randint(0, input_matrix_size - 1)
 
-    # step 9
-    equation_solver_9 = equation_solver(inputMatrixSize)
-    equation_solver_9.set_inverse_matrix(matrix_3)
+    #step 5
+    random_row_vector_5_1 = 10 * np.random.normal(size=input_matrix_size)
+    random_column_vector_5_2 = 10 * np.random.normal(size=input_matrix_size)
 
-    index_to_replace = random.randint(0, inputMatrixSize - 1)
-
-    random_row_vector_5_1 = 10 * np.random.normal(size=inputMatrixSize)
-    random_column_vector_5_2 = 10 * np.random.normal(size=inputMatrixSize)
     # step 6
-    inputMatrix[:, index_to_replace] = random_column_vector_5_2
-    inputMatrix[index_to_replace, :] = random_row_vector_5_1
+    input_matrix[:, index_to_replace] = random_column_vector_5_2
+    input_matrix[index_to_replace, :] = random_row_vector_5_1
 
     # step 7
     start_time = time.time()
-    matrix_7 = np.linalg.inv(inputMatrix)
+    matrix_7 = np.linalg.inv(input_matrix)
     # step 8
-    matrix_2 = np.dot(matrix_7, vector_2)
-    numpy_algorithm_time += time.time() - start_time
+    result_8 = np.dot(matrix_7, vector_2)
 
-    start_time = time.time()
     # step 10
-    equation_solver_9._replace_equation(index_to_replace, index_to_replace, random_row_vector_5_1,
-                                        random_column_vector_5_2)
-    result_11 = equation_solver_9.resolve(vector_2)
-    new_algorithm_time += time.time() - start_time
+    equation_solver_parameter.replace_equation(index_to_replace, index_to_replace, random_row_vector_5_1, random_column_vector_5_2)
+    result_11 = equation_solver_parameter.resolve(vector_2)
 
-    print("Iteration:", i, " Are Numpy and new Algorithm results the same? :", np.allclose(matrix_2, result_11))
+    print("Iteration:", i, " Are Numpy and new Algorithm results the same? :", np.allclose(result_8, result_11))
 
-    # print("Numpy algorithm took ", numpy_algorithm_time, " seconds")
-    # print("New algorithm took ", new_algorithm_time, " seconds")
-    size_comparison = numpy_algorithm_time / new_algorithm_time
-    print("** New algorithm is ", abs(round((size_comparison - 1) * 100, 1)), "%",
-          'faster' if (size_comparison >= 1) else 'slower')
+    return input_matrix;
 
-    return equation_solver_9.get_inverse();
-
-def test_add(inputMatrix):
+def test_add(input_matrix, equation_solver_parameter):
     print(' testing add equation')
-    inputMatrixSize = len(inputMatrix)
-    numpy_algorithm_time = 0
-    new_algorithm_time = 0
-    vector_2 = 10 * np.random.normal(size=inputMatrixSize + 1)
+    input_matrix_size = len(input_matrix)
+    vector_2 = 10 * np.random.normal(size=input_matrix_size + 1)
 
-    matrix_3 = np.linalg.inv(inputMatrix)
+    index_to_replace = random.randint(0, input_matrix_size - 1)
 
-    index_to_replace = 0  # random.randint(0, matrix_size - 1)
-
-    random_row_vector_5_1 = 10 * np.random.normal(size=inputMatrixSize + 1)
-    random_column_vector_5_2 = 10 * np.random.normal(size=inputMatrixSize + 1)
+    random_row_vector_5_1 = 10 * np.random.normal(size=input_matrix_size + 1)
+    random_column_vector_5_2 = 10 * np.random.normal(size=input_matrix_size + 1)
 
     # expand matrix by 1 row and 1 column
-    matrix_2 = np.eye(inputMatrix.shape[0] + 1)
+    matrix_2 = np.eye(input_matrix.shape[0] + 1)
 
-    matrix_2[:index_to_replace, :index_to_replace] = inputMatrix[:index_to_replace, :index_to_replace]
-    matrix_2[:index_to_replace, index_to_replace + 1:] = inputMatrix[:index_to_replace, index_to_replace:]
-    matrix_2[index_to_replace + 1:, :index_to_replace] = inputMatrix[index_to_replace:, :index_to_replace]
-    matrix_2[index_to_replace + 1:, index_to_replace + 1:] = inputMatrix[index_to_replace:, index_to_replace:]
+    matrix_2[:index_to_replace, :index_to_replace] = input_matrix[:index_to_replace, :index_to_replace]
+    matrix_2[:index_to_replace, index_to_replace + 1:] = input_matrix[:index_to_replace, index_to_replace:]
+    matrix_2[index_to_replace + 1:, :index_to_replace] = input_matrix[index_to_replace:, :index_to_replace]
+    matrix_2[index_to_replace + 1:, index_to_replace + 1:] = input_matrix[index_to_replace:, index_to_replace:]
     res22 = np.linalg.inv(matrix_2)
     matrix_2[:, index_to_replace] = random_column_vector_5_2
     matrix_2[index_to_replace, :] = random_row_vector_5_1
@@ -93,50 +73,35 @@ def test_add(inputMatrix):
     start_time = time.time()
     matrix_7 = np.linalg.inv(matrix_2)
     # step 8
-    matrix_2 = np.dot(matrix_7, vector_2)
-    numpy_algorithm_time += time.time() - start_time
-
-    # step 9
-    equation_solver_9 = equation_solver(inputMatrixSize * 2)
-    equation_solver_9.set_inverse_matrix(matrix_3)
+    result_8 = np.dot(matrix_7, vector_2)
 
     start_time = time.time()
 
-    equation_solver_9.add_equation(index_to_replace, index_to_replace, random_row_vector_5_1, random_column_vector_5_2)
+    equation_solver_parameter.add_equation(index_to_replace, index_to_replace, random_row_vector_5_1, random_column_vector_5_2)
 
-    result_11 = equation_solver_9.resolve(vector_2)
+    result_11 = equation_solver_parameter.resolve(vector_2)
 
-    new_algorithm_time += time.time() - start_time
+    print("Are Numpy and new Algorithm results the same? :", np.allclose(result_8, result_11))
 
-    print("Are Numpy and new Algorithm results the same? :", np.allclose(matrix_2, result_11))
-
-    # print("Numpy algorithm took ", numpy_algorithm_time, " seconds")
-    # print("New algorithm took ", new_algorithm_time, " seconds")
-    size_comparison = numpy_algorithm_time / new_algorithm_time
-    print("** New algorithm is ", abs(round((size_comparison - 1) * 100, 1)), "%",
-          'faster' if (size_comparison >= 1) else 'slower')
-    #
     # print("results using numpy:\n",result_8)
     # print("results using add equation:\n",result_11)
-    return equation_solver_9.get_inverse();
+    return matrix_2;
 
-def test_remove(inputMatrix):
+def test_remove(input_matrix, equation_solver_parameter):
     print(' testing remove equation')
 
-    inputMatrixSize = len(inputMatrix)
+    input_matrix_size = len(input_matrix)
 
-    vector_2 = 10 * np.random.normal(size=inputMatrixSize - 1)
+    vector_2 = 10 * np.random.normal(size=input_matrix_size - 1)
 
-    matrix_3 = np.linalg.inv(inputMatrix)
-
-    index_to_replace = random.randint(0, inputMatrixSize - 1)
+    index_to_replace = random.randint(0, input_matrix_size - 1)
 
     # removing row and column
-    matrix_2 = np.zeros([len(inputMatrix) - 1, len(inputMatrix) - 1])
-    matrix_2[:index_to_replace, :index_to_replace] = inputMatrix[:index_to_replace, :index_to_replace]
-    matrix_2[:index_to_replace, index_to_replace:] = inputMatrix[:index_to_replace, index_to_replace + 1:]
-    matrix_2[index_to_replace:, :index_to_replace] = inputMatrix[index_to_replace + 1:, :index_to_replace]
-    matrix_2[index_to_replace:, index_to_replace:] = inputMatrix[index_to_replace + 1:, index_to_replace + 1:]
+    matrix_2 = np.zeros([len(input_matrix) - 1, len(input_matrix) - 1])
+    matrix_2[:index_to_replace, :index_to_replace] = input_matrix[:index_to_replace, :index_to_replace]
+    matrix_2[:index_to_replace, index_to_replace:] = input_matrix[:index_to_replace, index_to_replace + 1:]
+    matrix_2[index_to_replace:, :index_to_replace] = input_matrix[index_to_replace + 1:, :index_to_replace]
+    matrix_2[index_to_replace:, index_to_replace:] = input_matrix[index_to_replace + 1:, index_to_replace + 1:]
 
     # inverse matrix
     matrix_7 = np.linalg.inv(matrix_2)
@@ -144,20 +109,30 @@ def test_remove(inputMatrix):
     # dot on rhs
     result_8 = np.dot(matrix_7, vector_2)
 
-    equation_solver_test = equation_solver(len(inputMatrix))
-    equation_solver_test.set_inverse_matrix(matrix_3)
-    equation_solver_test.remove_equation(index_to_replace, index_to_replace)
-    equation_solver_result = equation_solver_test.resolve(vector_2)
+    equation_solver_parameter.remove_equation(index_to_replace, index_to_replace)
+    equation_solver_result = equation_solver_parameter.resolve(vector_2)
     print("Are Numpy and new Algorithm results the same? :", np.allclose(result_8, equation_solver_result))
     #
     # print('result_8              :',result_8)
     # print('equation_solver_result:',equation_solver_result)
-    return equation_solver_test.get_inverse();
+    return matrix_2;
+
+
+#0 create test matrix
 
 test_matrix = 10 * np.random.rand(matrix_size, matrix_size)
+#1 inverse matrix
+
+inverse_of_test_matrix = np.linalg.inv(test_matrix)
+#2 input the inverse matrix to solver
+equation_solver_test = equation_solver(len(inverse_of_test_matrix) * 2)
+equation_solver_test.set_inverse_matrix(inverse_of_test_matrix)
 
 for i in range(times_to_run):
+    # 3 in each iteration, take 2 arguments - original matrix before inverse, and equation equation_solver
+    # 4 output of method is matrix that was changed using numpy operations
+    # 5 next method takes the matrix from previous output and pass original equation solver instance to it
 
-    test_matrix = test_replace(test_matrix)
-    test_matrix = test_add(test_matrix)
-    test_matrix = test_remove(test_matrix)
+    test_matrix = test_replace(test_matrix, equation_solver_test)
+    test_matrix = test_add(test_matrix, equation_solver_test)
+    test_matrix = test_remove(test_matrix, equation_solver_test)
