b77c160723921c9c3531e952479964fc9532fb47
Evgeny Shindin <>
2019-08-25 10:23:44 +0300

    comments on eq_order

--- SCLPsolver/subroutines/equation_order.py
+++ SCLPsolver/subroutines/equation_order.py
@@ -3,6 +3,10 @@ import numpy as np
 
 class equation_order():
 
+    # To identify equations (rows) we use 2 parameters:
+    #   var_names: names of variables that leave basis providing new equation
+    #   out_bases: basis number after which variable leave
+    # To identify columns we still using col_order
     __slots__ = ['_out_bases', '_var_names', '_col_order']
 
     def __init__(self):
@@ -10,6 +14,13 @@ class equation_order():
         self._var_names = []
         self._col_order = []
 
+    # This handles collision type I: we need to remove all bases between n1 and n2
+    # That is all columns between n1 and n2 should be removed
+    # Also we need to remove corresponding equations (rows)
+    # There are 2 main cases:
+    #   1. bases at the beginning or bases at the end should be removed (n1 == -1 or n2 not in self._col_order)
+    #   2. bases in the middle should be removed: in this case var_name is a variable leaving on pivot n1->n2 after removal
+    # Returns indexes of rows and columns that should be removed
     def remove(self, n1, n2, var_name=None):
         rows_to_remove = []
         cols_to_remove = []
@@ -38,71 +49,14 @@ class equation_order():
                     self._var_names[i] = 0
         return rows_to_remove, cols_to_remove
 
-    # def insert_row_to_cols(self, vv, dx, dq, klist, jlist, pivots, cols, n1, n2, n):
-    #     if vv > 0:
-    #         k = find(klist == vv)[0]
-    #         prev_in = pivots.get_previous_in(n)
-    #         if prev_in is None:
-    #             prev_in = -1
-    #         start = max(n1 + 1, prev_in + 1)
-    #         end = n + 1
-    #         if start < end:
-    #             cols[n, start-n1:end-n1] = dx[k, start:end]
-    #     else:
-    #         j = find(jlist == -vv)[0]
-    #         next_in = pivots.get_next_in(n)
-    #         if next_in is None:
-    #             next_in = n2 -1
-    #         start = n + 1
-    #         end = min(next_in + 1, n2)
-    #         if start < end:
-    #             cols[n, start-n1:end-n1] = dq[j, start:end]
-
-    # def insert_at_start(self, var_names):
-    #     m = 0
-    #     n1 = -1
-    #     corr = 1
-    #     cols_to_insert = []
-    #     rows_to_insert = []
-    #     cols = np.zeros((len(self._col_order)+len(var_names) - 1 + corr,len(var_names) - 1 + corr))
-    #     cols[0,:] = np.ones(len(var_names) - 1 + corr)
-    #     rows = np.zeros((len(var_names) - 1 + corr,len(self._col_order)+len(var_names) - 1 + corr))
-    #     for i, v in enumerate(self._col_order):
-    #         if v == -1:
-    #             if m < len(var_names):
-    #                 m += 1
-    #                 self._col_order[i] = n1 + m
-    #                 cols_to_insert.append(i)
-    #         elif v > n1:
-    #             self._col_order[i] = v + len(var_names) - 1 + corr
-    #     for j in range(m, len(var_names) - 1 + corr):
-    #         cols_to_insert.append(len(self._col_order))
-    #         self._col_order.append(n1 + 1 + j)
-    #     m = 0
-    #     ind_v = None
-    #     for i, v in enumerate(self._out_bases):
-    #         if v == -1:
-    #             if m == ind_v:
-    #                 m += 1
-    #             if m < len(var_names):
-    #                 self._out_bases[i] = n1 + m + corr
-    #                 self._var_names[i] = var_names[m]
-    #                 rows_to_insert.append(i)
-    #                 #self.insert_row_to_cols(self._var_names[i], dx, dq, klist, jlist, pivots, cols, n1, n1+len(var_names) - 1 + corr, n1 + m + corr)
-    #                 m += 1
-    #         elif v > n1:
-    #             self._out_bases[i] = v + len(var_names) - 1 + corr
-    #         elif v == n1 and ind_v is not None:
-    #             self._out_bases[i] = n1 + ind_v
-    #     for j in range(m, len(var_names) - 1 + corr):
-    #         if j != ind_v:
-    #             rows_to_insert.append(len(self._out_bases))
-    #             self._out_bases.append(n1 + j + corr)
-    #             self._var_names.append(var_names[j])
-    #             #self.insert_row_to_cols(var_names[j], dx, dq, klist, jlist, pivots, cols, n1, n1 + len(var_names) - 1 + corr, n1 + j + corr)
-    #     return rows_to_insert, cols_to_insert
-
-
+    # This handles collision type III: we need to insert bases between n1 and n1+1
+    # That is we need to insert columns between n1 and n1+1
+    # Also we need to insert corresponding equations (rows)
+    # There are 2 main cases:
+    #   1. we need to insert bases at the beginning or bases at the end (n1 == -1 or n1 not in self._out_bases)
+    #   2. we need to insert bases in the middle
+    # var_names handles variables leaving on pivots n1->n1+1, n1+1->n1+2,..., n1+m->n2 after insertion
+    # Returns indexes of rows and columns that should be inserted
     def insert(self, n1, var_names):
         cols_to_insert = []
         rows_to_insert = []
@@ -145,7 +99,16 @@ class equation_order():
                 self._var_names.append(var_names[j])
         return rows_to_insert, cols_to_insert
 
-    # ar_names include all out_pivots (both n1-> and ->n2)
+    # This handles collision type II: we need to replace bases between n1 and n2
+    # The number of new bases could be less or greater then number of old bases (n2 - n1 - 1)
+    # var_names include all leaving variable names n1->n1+1, n1+1->n1+2,..., n1+m->n2 after replacement
+    # v1 and v2 includes names of two variables that are differs between n1 and n2 before (and after) replacement
+    # this helps to identify equations (rows) that should not be replaced
+    # Returns:
+    #   cols_to_replace - indexes of columns that should be replaced
+    #   cols_to_ar - indexes of columns that should be inserted or removed
+    #   rows_to_ar - indexes of rows that should be replaced or removed
+    #   need_to_add; True/False if we need to add rows and columns
     def replace(self, n1, n2, v1, v2, var_names):
         cols_to_replace = []
         cols_to_ar = []
