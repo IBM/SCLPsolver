1b081b81daf89761de568b0f721d3a3e9e94d005
Harold Ship <harold@il.ibm.com>
2021-02-18 13:31:19 +0200

    update MCQN_basic_test.py
      parameterize tests to use multiple seeds

--- SCLPsolver/pytests/MCQN_basic_test.py
+++ SCLPsolver/pytests/MCQN_basic_test.py
@@ -8,47 +8,54 @@ from SCLPsolver.SCLP import SCLP, SCLP_settings
 from SCLPsolver.doe.data_generators.MCQN import generate_MCQN_data
 
 
-def test_basic_mcqn():
+seeds = range(1001, 1009)
+
+
+@pytest.mark.parametrize("seed", seeds)
+def test_basic_mcqn(seed):
     K = 400
     I = 40
     import time
-    solver_settings = SCLP_settings(find_alt_line=False, check_intermediate_solution=False,  memory_management= False, suppress_printing = False)
+    solver_settings = SCLP_settings(find_alt_line=False, check_intermediate_solution=False, memory_management=False,
+                                    suppress_printing=False)
 
-    settings = {'alpha_rate':  1, 'cost_scale':2, 'a_rate' : 0.05, 'sum_rate':0.95, 'nz': 0.5,
-                        'gamma_rate':0, 'c_scale': 0, 'h_rate': 0.2}
-    seed = 1009
+    settings = {'alpha_rate': 1, 'cost_scale': 2, 'a_rate': 0.05, 'sum_rate': 0.95, 'nz': 0.5,
+                'gamma_rate': 0, 'c_scale': 0, 'h_rate': 0.2}
     G, H, F, gamma, c, d, alpha, a, b, TT, total_buffer_cost, buffer_cost = generate_MCQN_data(seed, K, I, **settings)
     TT = 100
 
     result = {'servers': I, 'buffers': K, 'seed': seed}
     start_time = time.time()
-    solution, STEPCOUNT, param_line, res = SCLP(G, H, F, a, b, c, d, alpha, gamma, 3/12 * TT, solver_settings)
+    solution, STEPCOUNT, param_line, res = SCLP(G, H, F, a, b, c, d, alpha, gamma, 3 / 12 * TT, solver_settings)
     t, x, q, u, p, pivots, obj, err, NN, tau, maxT = solution.get_final_solution(True)
 
-    assert obj is not None
-    assert 0 < maxT < TT
-    assert len(t) > 0
+    assert obj is not None, "solution obj is None!"
+    assert 0 < maxT < TT, f"0 < {maxT} < {TT} failed"
+    assert len(t) > 0, f"len(t) is {len(t)}"
 
 
-def test_degenerate_mcqn():
+@pytest.mark.parametrize("seed", seeds)
+def test_degenerate_mcqn(seed):
     K = 400
     I = 40
     import time
-    solver_settings = SCLP_settings(find_alt_line=False, check_intermediate_solution=False,  memory_management= False, suppress_printing = False)
+    solver_settings = SCLP_settings(find_alt_line=False, check_intermediate_solution=False, memory_management=False,
+                                    suppress_printing=False)
 
-    settings = {'alpha_rate':  1, 'cost_scale':2, 'a_rate' : 0.05, 'sum_rate':0.95, 'nz': 0.5,
-                        'gamma_rate':0, 'c_scale': 0, 'h_rate': 0.2}
-    seed = 1009
+    settings = {'alpha_rate': 1, 'cost_scale': 2, 'a_rate': 0.05, 'sum_rate': 0.95, 'nz': 0.5,
+                'gamma_rate': 0, 'c_scale': 0, 'h_rate': 0.2}
     G, H, F, gamma, c, d, alpha, a, b, TT, total_buffer_cost, buffer_cost = generate_MCQN_data(seed, K, I, **settings)
-    a[0:4] = [0,0,0,0]
-    c[6:8] = [0,0]
+    a[0:4] = [0, 0, 0, 0]
+    c[6:8] = [0, 0]
     TT = 100
 
     result = {'servers': I, 'buffers': K, 'seed': seed}
     start_time = time.time()
-    solution, STEPCOUNT, param_line, res = SCLP(G, H, F, a, b, c, d, alpha, gamma, 3/12 * TT, solver_settings)
+    solution, STEPCOUNT, param_line, res = SCLP(G, H, F, a, b, c, d, alpha, gamma, 3 / 12 * TT, solver_settings)
     t, x, q, u, p, pivots, obj, err, NN, tau, maxT = solution.get_final_solution(True)
 
-    assert obj is not None
-    assert 0 < maxT < TT
-    assert len(t) > 0
+    assert obj is not None, "solution obj is None!"
+    assert 0 < maxT < TT, f"0 < {maxT} < {TT} failed"
+    assert len(t) > 0, f"len(t) is {len(t)}"
+
+
